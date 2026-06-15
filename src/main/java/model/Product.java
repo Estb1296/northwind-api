c08751f8ad09d8abd369a18a2953dacccf23aeb5
ezrateshale <ezra.teshale12@gmail.com>
2026-06-15 15:44:35 -0500

    feat: Model and Repository complete

---
+++ src/main/java/model/Product.java
@@ -0,0 +1,126 @@
+package model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @NotBlank
+    private String productName;
+
+    @ManyToOne
+    @JoinColumn(name = "supplierId")
+    private Long supplierId;
+
+    @ManyToOne
+    @JoinColumn(name = "categoryId")
+    private Long categoryId;
+
+    private String quantityPerUnit;
+
+    private BigDecimal unitPrice;
+
+    @PositiveOrZero
+    private int unitsInStock;
+
+    @PositiveOrZero
+    private int unitsOnOrder;
+
+    @PositiveOrZero
+    private int reorderLevel;
+
+    @Column(name = "discontinued")
+    private Boolean discontinued;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public Long getSupplierId() {
+        return supplierId;
+    }
+
+    public void setSupplierId(Long supplierId) {
+        this.supplierId = supplierId;
+    }
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getQuantityPerUnit() {
+        return quantityPerUnit;
+    }
+
+    public void setQuantityPerUnit(String quantityPerUnit) {
+        this.quantityPerUnit = quantityPerUnit;
+    }
+
+    public BigDecimal getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(BigDecimal unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public int getUnitsInStock() {
+        return unitsInStock;
+    }
+
+    public void setUnitsInStock(int unitsInStock) {
+        this.unitsInStock = unitsInStock;
+    }
+
+    public int getUnitsOnOrder() {
+        return unitsOnOrder;
+    }
+
+    public void setUnitsOnOrder(int unitsOnOrder) {
+        this.unitsOnOrder = unitsOnOrder;
+    }
+
+    public int getReorderLevel() {
+        return reorderLevel;
+    }
+
+    public void setReorderLevel(int reorderLevel) {
+        this.reorderLevel = reorderLevel;
+    }
+
+    public Boolean getDiscontinued() {
+        return discontinued;
+    }
+
+    public void setDiscontinued(Boolean discontinued) {
+        this.discontinued = discontinued;
+    }
+
+
+}
